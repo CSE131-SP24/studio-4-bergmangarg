deb23e05283eb4ae3e3983ff6d17185ad1dd01e5
TheStormfly <TheStormfly@localhost>
2024-02-21 15:46:33 -0600

    Fluffy!

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,12 +6,21 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		int red = 0;
-		int green = 0;
-		int blue = 0;
-		StdDraw.
-				
-				
-		
+		StdDraw.clear();
+		StdDraw.setPenColor(160,44,219);
+		StdDraw.filledRectangle(.15, .15, .15, .15);
+		StdDraw.filledRectangle(.85, .15, .15, .15);
+		StdDraw.filledRectangle(.15, .85, .15, .15);
+		StdDraw.filledRectangle(.85, .85, .15, .15);	
+		StdDraw.setPenColor(73,44,219);
+		StdDraw.filledRectangle(.5, .5, .2, .2);
+		StdDraw.setPenColor(44,103,219);
+		StdDraw.filledRectangle(.5, .15, .2, .15);
+		StdDraw.filledRectangle(.5, .85, .2, .15);
+		StdDraw.filledRectangle(.15, .5, .15, .2);
+		StdDraw.filledRectangle(.85, .5, .15, .2);
+		StdDraw.pause(1000);
+		StdDraw.setPenColor(255,255,255);
+		StdDraw.text(.5, .5, "Fluffy");
 	}
 }
