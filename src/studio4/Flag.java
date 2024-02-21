03eb57491cd3a9f7665510aab0692e2b7a291600
TheStormfly <TheStormfly@localhost>
2024-02-21 15:21:09 -0600

    Done interpret file

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,12 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		int red = 0;
+		int green = 0;
+		int blue = 0;
+		StdDraw.
+				
+				
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,40 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String shapeType = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		StdDraw.clear();
+		StdDraw.setPenColor(red, green, blue);
+		double p1 = in.nextDouble();
+		double p2 = in.nextDouble();
+		double p3 = in.nextDouble();
+		double p4 = in.nextDouble();
+		if (shapeType.equals("rectangle")) {
+			if (isFilled) {
+				StdDraw.filledRectangle(p1, p2, p3, p4);
+			} else {
+				StdDraw.rectangle(p1, p2, p3, p4);
+			}
+		}
+		if (shapeType.equals("ellipse")) {
+			if(isFilled) {
+				StdDraw.filledEllipse(p1, p2, p3, p4);
+			}else {
+				StdDraw.ellipse(p1, p2, p3, p4);
+			}
+		} if (shapeType.equals("triangle")) {
+			double p5 = in.nextDouble();
+			double p6 = in.nextDouble();
+			double [] px = {p1, p3, p5};
+			double [] py = {p2, p4, p6};
+			if(isFilled) {
+				StdDraw.filledPolygon(px, py);
+			} else {
+				StdDraw.polygon(px, py);
+			}
+		}
 	}
 }
